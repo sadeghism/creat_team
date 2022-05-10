10c24b02385c3832e989816f375e83c8e0890282
mohammad <sadeghism@yahoo.com>
2022-05-10 10:04:18 +0430

    done register system

--- app/http/controller/auth.controller.js
+++ app/http/controller/auth.controller.js
@@ -1,9 +1,12 @@
 const {validationResult} = require('express-validator');
+const { UserModel } = require('../../model/userModel');
+const { hashPassword } = require('../../module/functions');
 const { expressValidatorMapper } = require('../middleware/checkErrorValidator');
 class AuthController {
   register(req, res, next) {
     const { username, mobile, email, password } = req.body;
-    return res.json(req.body);
+    const result = await UserModel.create({username , email , mobile , password:hashPassword});
+    return res.json(result);
   }
   login() {}
   resetPassword() {}
